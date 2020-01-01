6139f67bcbedb2b4613270d70717c6d434cd9599
elma guo <1054522381@qq.com>
2020-01-01 23:13:07 +0800

    函数 & 字符串

--- 06_函数.py
+++ 06_函数.py
@@ -1,5 +1,6 @@
 # 定义函数
 def print_stars():
+    """鼠标放在函数调用的位置，按ctrl+Q，函数文档在这里写"""
     i = 1
     while i <= 5:
         print('*' * i)
@@ -9,8 +10,21 @@ def print_stars():
 # 函数调用
 print_stars()
 
+# help查看函数文档
+print(help(print_stars))
 
+
+# 定义带缺省参数的函数，缺省参数位置之后定义的形式参数都必须带缺省值
 def add(a, b=10):
+    """在这里简单描述函数功能
+
+    :param int a: 加法操作数
+    :param int b: 加法操作数
+    :return: 返回值
+    """
+    if not isinstance(a, int) or not isinstance(b, int):
+        print('参数类型应该为数字！')
+        return None
     print('a + b = %d' % (a + b))
     return a + b
 
@@ -20,3 +34,7 @@ add(1, 2)
 print('add(1) result=%d' % add(1))
 # 关键字参数，根据参数名传参
 add(b=1, a=4)
+# 既有位置参数又有关键字参数，位置参数需要放在关键字参数前面
+add(5, b=5)
+
+add(None)
---
+++ 07_字符串.py
@@ -0,0 +1,50 @@
+# 字符串定义
+my_str = 'hello@itcast.com'
+my_str = "world@itcast.com"
+# my_str = """
+#     保留制表符和
+#     换行
+# """
+print(my_str)
+
+# 用正负下标值访问字符
+print(my_str[0])
+print(my_str[-1])
+
+# 访问子字符串 [start, end)
+print(my_str[1:5])
+# 步长
+print('根据步长取子串：', my_str[1:5:2])
+# 字符串逆序
+print('逆序：', my_str[::-1])
+
+print('@字符下标是：', my_str.find('@'))
+
+print('字符串长度:', len(my_str))
+
+print('重复字符串:', my_str * 2)
+
+# 遍历
+for char in my_str:
+    print(char, end=' ')
+print()
+
+# 字符串替换
+print((my_str * 2).replace('@', '#', 1))
+
+# 拆分
+print(my_str.split('@'))
+
+# 计算子串出现的次数
+print('@出现的次数：', my_str.count('@'))
+
+# 去除两端空格
+print(' hello world   '.strip())
+
+# 判断是否全字母
+print(my_str.isalpha())
+print('是否全字母：', 'hello'.isalpha())
+
+# 判断全数字
+print('是否全数字', '3534587'.isdigit())
+
